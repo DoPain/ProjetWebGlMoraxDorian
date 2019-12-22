4c85b83b3f7e50ae86b3000266f2665790d08eeb
Morax Dorian <dorian.morax@etu.u-bordeaux.fr>
2019-12-22 17:07:54 +0100

    ajout du suivis de curseur

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-import * as THREE from '../vendor/three.js-master/build/three.module.js';
-import Stats from '../vendor/three.js-master/examples/jsm/libs/stats.module.js';
-import { OrbitControls } from '../vendor/three.js-master/examples/jsm/controls/OrbitControls.js';
-import { FBXLoader } from '../vendor/three.js-master/examples/jsm/loaders/FBXLoader.js';
-
 (function() {
     let scene,
         renderer,
@@ -54,10 +49,23 @@ function init() {
             let fileAnimations = g.animations;
 
             scene.add(model);
+            loaderAnim.remove();
+            mixer = new THREE.AnimationMixer(model);
+            let idleAnim = THREE.AnimationClip.findByName(fileAnimations, 'idle');
+            idleAnim.tracks.splice(3, 3);
+            idleAnim.tracks.splice(9, 3);
+            idle = mixer.clipAction(idleAnim);
+            idle.play();
             model.traverse(o => {
                 if (o.isMesh) {
                     o.castShadow = true;
                     o.receiveShadow = true;
+                }
+                if (o.isBone && o.name === 'mixamorigNeck') {
+                    neck = o;
+                }
+                if (o.isBone && o.name === 'mixamorigSpine') {
+                    waist = o;
                 }
             });
             model.scale.set(7, 7, 7);
@@ -107,6 +115,9 @@ function init() {
 }
 
 function update() {
+    if (mixer) {
+        mixer.update(clock.getDelta());
+    }
     if (resizeRendererToDisplaySize(renderer)) {
         const canvas = renderer.domElement;
         camera.aspect = canvas.clientWidth / canvas.clientHeight;
@@ -115,6 +126,8 @@ function update() {
     renderer.render(scene, camera);
     requestAnimationFrame(update);
 }
+
+update();
 
 function resizeRendererToDisplaySize(renderer) {
     const canvas = renderer.domElement;
@@ -131,5 +144,62 @@ function resizeRendererToDisplaySize(renderer) {
     return needResize;
 }
 
+document.addEventListener('mousemove', function(e) {
+    var mousecoords = getMousePos(e);
+    if (neck && waist) {
+        moveJoint(mousecoords, neck, 50);
+        moveJoint(mousecoords, waist, 30);
+    }
+});
+
+function getMousePos(e) {
+    return { x: e.clientX, y: e.clientY };
+}
+
+function moveJoint(mouse, joint, degreeLimit) {
+    let degrees = getMouseDegrees(mouse.x, mouse.y, degreeLimit);
+    joint.rotation.y = THREE.Math.degToRad(degrees.x);
+    joint.rotation.x = THREE.Math.degToRad(degrees.y);
+}
+
+function getMouseDegrees(x, y, degreeLimit) {
+    let dx = 0,
+        dy = 0,
+        xdiff,
+        xPercentage,
+        ydiff,
+        yPercentage;
+
+    let w = { x: window.innerWidth, y: window.innerHeight };
+
+    // 1. Si le curseur est sur la partie gauche de l'ecran
+    if (x <= w.x / 2) {
+        // 2. Différence entre le milieu de l'écran et la position de la souris
+        xdiff = w.x / 2 - x;
+        // 3. Pourcentage de cette différence
+        xPercentage = (xdiff / (w.x / 2)) * 100;
+        // 4. Convertissons cela en un pourcentage de la rotation maximale que nous autorisons pour le cou
+        dx = ((degreeLimit * xPercentage) / 100) * -1; }
+// Droite
+    if (x >= w.x / 2) {
+        xdiff = x - w.x / 2;
+        xPercentage = (xdiff / (w.x / 2)) * 100;
+        dx = (degreeLimit * xPercentage) / 100;
+    }
+    // Haut
+    if (y <= w.y / 2) {
+        ydiff = w.y / 2 - y;
+        yPercentage = (ydiff / (w.y / 2)) * 100;
+        // Note that I cut degreeLimit in half when she looks up
+        dy = (((degreeLimit * 0.5) * yPercentage) / 100) * -1;
+    }
+
+    // Bas
+    if (y >= w.y / 2) {
+        ydiff = y - w.y / 2;
+        yPercentage = (ydiff / (w.y / 2)) * 100;
+        dy = (degreeLimit * yPercentage) / 100;
+    }
+    return { x: dx, y: dy };
+}
 init();
-update();
